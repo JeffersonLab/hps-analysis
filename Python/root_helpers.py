b19d98b6f36193169486db1846aa18523c61b7b3
Maurik Holtrop <maurik@physics.unh.edu>
2021-08-04 12:35:40 +0200

    Modified style

--- Python/root_helpers.py
+++ Python/root_helpers.py
@@ -7,7 +7,8 @@ def print_daughters(mdst, i_part, indent=0):
         ident  -- the amount of indentation of the output.
     """
 
-    print(" "*indent+f" {i_part:3d}  pdg: {mdst.mc_part_pdg_id[i_part]:4d}  E: {mdst.mc_part_energy[i_part]:9.6f} ")
+    print(" "*indent+f" {i_part:3d}  pdg: {mdst.mc_part_pdg_id[i_part]:4d}  E: {mdst.mc_part_energy[i_part]:9.6f} " +
+    f"p = ({mdst.mc_part_px[i_part]:9.6f},{mdst.mc_part_py[i_part]:9.6f},{mdst.mc_part_pz[i_part]:9.6f})"          )
     if len(mdst.mc_part_daughters[i_part]) > 0:
         print(" "*(indent+14) + "| ")
         for i in range(len(mdst.mc_part_daughters[i_part])):
@@ -24,27 +25,23 @@ def print_mc_particle_tree(mdst):
             print_daughters(mdst, i, 0)
 
 
-def SetStyle():
+def SetStyle(choice=0):
     if "R" not in globals():
         import ROOT as R
     if "gROOT" not in globals():
         from ROOT import gROOT
-    if "gSystem" not in globals():
-        from ROOT import gSystem
 
-    # gROOT.SetBatch(1)
-
-    hpsStyle= R.TStyle("HPS", "HPS style")
+    hpsStyle = R.TStyle("HPS", "HPS style")
 
     # use plain black on white colors
-    icol=0
+    icol = 0
     hpsStyle.SetFrameBorderMode(icol)
     hpsStyle.SetCanvasBorderMode(icol)
     hpsStyle.SetPadBorderMode(icol)
     hpsStyle.SetPadColor(icol)
     hpsStyle.SetCanvasColor(icol)
     hpsStyle.SetStatColor(icol)
-    #hpsStyle.SetFillColor(icol)
+    # hpsStyle.SetFillColor(icol)
 
     # set the paper & margin sizes
     hpsStyle.SetPaperSize(20,26)
@@ -54,68 +51,75 @@ def SetStyle():
     hpsStyle.SetPadLeftMargin(0.14)
 
     # use large fonts
-    #font=72
-    font=42
-    tsize=0.08
-    tzsize = 0.045
+    # font=72
+    if(choice == 0):
+        font = 42         # helvetica-medium-r-normal
+        title_font = 42
+        title_size = 0.08
+        title_size_z = 0.045
+
+        label_font = 42
+        label_size = 0.08
+        label_size_z = 0.045
+        hpsStyle.SetOptTitle(0)
+        hpsStyle.SetOptStat(0)
+        hpsStyle.SetOptFit(0)
+
+    elif(choice == 1):
+        #font=72
+        font = 132        # times-medium-r-normal
+        title_font = 132
+        title_size = 0.08
+        title_size_z = 0.045
+
+        label_font = 42
+        label_size = 0.035
+        label_size_z = 0.035
+        hpsStyle.SetOptTitle(0)
+        hpsStyle.SetOptStat(0)
+        hpsStyle.SetOptFit(0)
+
+
     hpsStyle.SetTextFont(font)
+    hpsStyle.SetTextSize(label_size)
 
+    hpsStyle.SetLabelFont(label_font, "x")
+    hpsStyle.SetTitleFont(title_font, "x")
+    hpsStyle.SetLabelFont(label_font, "y")
+    hpsStyle.SetTitleFont(title_font, "y")
+    hpsStyle.SetLabelFont(label_font, "z")
+    hpsStyle.SetTitleFont(title_font, "z")
 
-    hpsStyle.SetTextSize(tsize)
-    hpsStyle.SetLabelFont(font,"x")
-    hpsStyle.SetTitleFont(font,"x")
-    hpsStyle.SetLabelFont(font,"y")
-    hpsStyle.SetTitleFont(font,"y")
-    hpsStyle.SetLabelFont(font,"z")
-    hpsStyle.SetTitleFont(font,"z")
+    hpsStyle.SetLabelSize(label_size, "x")
+    hpsStyle.SetTitleSize(title_size, "x")
+    hpsStyle.SetLabelSize(label_size, "y")
+    hpsStyle.SetTitleSize(title_size, "y")
+    hpsStyle.SetLabelSize(label_size_z, "z")
+    hpsStyle.SetTitleSize(title_size_z, "z")
 
-    hpsStyle.SetLabelSize(tsize,"x")
-    hpsStyle.SetTitleSize(tsize,"x")
-    hpsStyle.SetLabelSize(tsize,"y")
-    hpsStyle.SetTitleSize(tsize,"y")
-    hpsStyle.SetLabelSize(tzsize,"z")
-    hpsStyle.SetTitleSize(tzsize,"z")
-
-    hpsStyle.SetTitleOffset(0.7,"y")
-    hpsStyle.SetTitleOffset(1.15,"x")
+    hpsStyle.SetTitleOffset(0.7, "y")
+    hpsStyle.SetTitleOffset(1.15, "x")
 
     #use bold lines and markers
     #hpsStyle.SetMarkerStyle(20)
     hpsStyle.SetMarkerSize(1.0)
-    hpsStyle.SetHistLineWidth(3)
-    hpsStyle.SetLineStyleString(2,"[12 12]") # postscript dashes
+    hpsStyle.SetHistLineWidth(1)
+    hpsStyle.SetLineStyleString(2, "[12 12]")  # postscript dashes
 
     #get rid of X error bars and y error bar caps
     #hpsStyle.SetErrorX(0.001)
 
     #do not display any of the standard histogram decorations
-    hpsStyle.SetOptTitle(0)
-    #hpsStyle.SetOptStat(1111)
-    hpsStyle.SetOptStat(0)
-    #hpsStyle.SetOptFit(1111)
-    hpsStyle.SetOptFit(0)
 
     # put tick marks on top and RHS of plots
     hpsStyle.SetPadTickX(1)
     hpsStyle.SetPadTickY(1)
 
-    gROOT.SetStyle("Plain")
-    #gStyle.SetPadTickX(1)
-    #gStyle.SetPadTickY(1)
+    #gROOT.SetStyle("Plain")
     gROOT.SetStyle("HPS")
     gROOT.ForceStyle()
-    R.gStyle.SetOptTitle(0)
-    R.gStyle.SetOptStat(0)
-    R.gStyle.SetOptFit(0)
 
-    # overwrite hps styles
-    hpsStyle.SetPadLeftMargin(0.14)
-    hpsStyle.SetPadRightMargin(0.06)
-    hpsStyle.SetPadBottomMargin(0.11)
-    hpsStyle.SetPadTopMargin(0.05)
-    hpsStyle.SetFrameFillColor(0)
-
-    NCont = 255;
+    NCont = 255
     stops = R.std.vector("double")([0.00, 0.34, 0.61, 0.84, 1.00])
     red = R.std.vector("double")([0.00, 0.00, 0.87, 1.00, 0.51])
     green = R.std.vector("double")([0.00, 0.81, 1.00, 0.20, 0.00])

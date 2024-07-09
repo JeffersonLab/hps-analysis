e15a3c68497aeebf45c2724cd44558790db008b1
Maurik Holtrop <maurik@physics.unh.edu>
2024-07-09 15:20:29 -0400

    Start filling in the Moller class

--- Moller/Moller/Moller.cxx
+++ Moller/Moller/Moller.cxx
@@ -28,10 +28,6 @@ void Moller::Process() {
     ///
     /// Trigger Processing of all the filter steps and print a summary.
     ///
-    if( filters.size() == 0){
-        std::cout << "Please run Setup() first.\n";
-        return;
-    }
     std::cout << "Start processing.\n";
     TStopwatch timer;
     timer.Start();
@@ -64,8 +60,127 @@ RNode Moller::Cut_2_electrons(RNode in, bool exact) {
       return (exact && n==2) || (!exact && n>=2);
    };
 
-   return in.Filter(cut_two_electrons,{"part_pdg"});
+   return in.Filter(cut_two_electrons, {"part_pdg"} );
 }
+
+RNode Moller::Select_El_Pairs(RNode in, double time_cut, std::string out_name){
+   // Select all pairs of electrons from the event that have track_time within time_cut = 2ns.
+   // Return the RDataFrame with an "electron_pairs" variable that are pair<int,int> type which are indexes to the part banks.
+
+   auto select_el_pairs = [time_cut](RVec<int> part_pdg, RVec<int> part_track, RVec<double> track_time)-> std::vector< std::pair<int,int>> {
+      std::vector< std::pair<int,int> > out;
+      for(int i=0; i<part_pdg.size(); ++i)
+         for(int j=i+1; j< part_pdg.size(); ++j){
+            if(part_pdg[i] == 11 && part_pdg[j] == 11){
+               int ii = part_track[i];
+               int jj = part_track[j];
+               if( abs(track_time[ii] - track_time[jj]) <  time_cut){
+                  out.emplace_back( std::make_pair(i,j));
+               }
+            }
+         }
+      return out;
+   };
+
+   return in.Define(out_name, select_el_pairs, {"part_pdg", "part_track", "track_time"});
+}
+
+RNode Moller::Anti_Select_El_Pairs(RNode in, double time_cut, std::string out_name){
+   // Select all pairs of electrons from the event that have track_time within time_cut = 2ns.
+   // Return the RDataFrame with an "electron_pairs" variable that are pair<int,int> type which are indexes to the part banks.
+
+   auto select_el_pairs = [time_cut](RVec<int> part_pdg, RVec<int> part_track, RVec<double> track_time)-> std::vector< std::pair<int,int>> {
+      std::vector< std::pair<int,int> > out;
+      for(int i=0; i<part_pdg.size(); ++i)
+         for(int j=i+1; j< part_pdg.size(); ++j){
+            if(part_pdg[i] == 11 && part_pdg[j] == 11){
+               int ii = part_track[i];
+               int jj = part_track[j];
+               if( abs(track_time[ii] - track_time[jj]) > time_cut){
+                  out.emplace_back( std::make_pair(i,j));
+               }
+            }
+         }
+      return out;
+   };
+
+   return in.Define(out_name, select_el_pairs, {"part_pdg", "part_track", "track_time"});
+}
+
+
+
+RNode Moller::Add_Momentum_Sum(RNode in, std::string in_name, std::string out_name){
+   // Add the momentum sum (p1 + p2).Mag() for each pair in in_name as out_name
+
+   auto add_psum = [](std::vector< std::pair<int, int> > pairs, RVec<double> part_px, RVec<double> part_py, RVec<double> part_pz ){
+      std::vector<double> out;
+      for(auto p: pairs){
+         TVector3 p1(part_px[p.first], part_py[p.first], part_pz[p.first]);
+         TVector3 p2(part_px[p.second], part_py[p.second], part_pz[p.second]);
+         double psum = (p1+p2).Mag();
+         out.push_back(psum);
+      }
+      return out;
+   };
+
+   return in.Define(out_name, add_psum, {in_name, "part_px", "part_py", "part_pz"});
+}
+
+RNode Moller::Refine_El_Pairs_1(RNode in, double pmin, double pmax, std::string in_name, std::string out_name){
+   // Momentum sum cut for the electron pairs found in "in_name" collection of pairs: pmin < p1+p2 < pmax
+   auto mom_sum_cut = [pmin, pmax](std::vector< std::pair<int, int> > pairs, RVec<double> part_px, RVec<double> part_py, RVec<double> part_pz ){
+      std::vector< std::pair<int,int> > out;
+      for(auto p: pairs){
+         TVector3 p1(part_px[p.first], part_py[p.first], part_pz[p.first]);
+         TVector3 p2(part_px[p.second], part_py[p.second], part_pz[p.second]);
+         double psum = (p1+p2).Mag();
+         if( pmin < psum && psum < pmax){
+            out.push_back(p);
+         }
+      }
+      return out;
+   };
+
+   return in.Define(out_name, mom_sum_cut, {in_name, "part_px", "part_py", "part_pz"});
+};
+
+RNode Moller::Refine_El_Pairs_2(RNode in, std::string in_name, std::string out_name){
+   // Cut on the electron pairs, requiring one in the top and one in the bottom volume of HPS:  tan_lambda_1 * tan_lambra_2 < 0 => part_py_1 * part_py_2 < 0
+
+   auto track_tan_lambda_cut = [](std::vector< std::pair<int, int> > pairs, RVec<double> part_py){
+      std::vector< std::pair<int,int> > out;
+      for(auto p: pairs){
+         if( part_py[p.first]*part_py[p.second] < 0 ){
+            out.push_back(p);
+         }
+      }
+      return out;
+   };
+
+   return in.Define(out_name, track_tan_lambda_cut, {in_name, "part_py"});
+};
+
+
+RNode Moller::Add_Moller_Inv_Mass(RNode in, std::string pair_name, std::string out_name){
+   // Add the invariant mass for the two electrons in the event.
+   // If there are more than two pairs, all combinatorics are used to compute the invariant mass.
+
+   auto compute_invariant_mass = [pair_name](std::vector<std::pair<int,int> > &ele_pairs, RVec<double> &px, RVec<double> &py, RVec<double> &pz){
+      std::vector<double> mass_out;
+      for(const std::pair<int, int> &p: ele_pairs){
+               TLorentzVector p1, p2;
+               p1.SetXYZM(px[p.first],py[p.first],pz[p.first],0.0005109989499961642);
+               p2.SetXYZM(px[p.second],py[p.second],pz[p.second],0.0005109989499961642);
+               mass_out.push_back( (p1+p2).M());
+      }
+      return mass_out;
+   };
+
+   return in.Define(out_name,compute_invariant_mass,{pair_name, "part_px", "part_py", "part_pz"});
+}
+
+
+
 
 void Moller::Print(Option_t *opt) {
 
--- Moller/Moller/Moller.h
+++ Moller/Moller/Moller.h
@@ -5,149 +5,128 @@
 #ifndef MOLLER_MOLLER_H
 #define MOLLER_MOLLER_H
 
-#define __MOLLER_VERSION__ "0.1.1"
+#define __MOLLER_VERSION__ "0.1.2"
 
 #include "TObject.h"
 #include "TChain.h"
 #include "TROOT.h"
+#include "TLorentzVector.h"
 #include "ROOT/RDataFrame.hxx"
 #include "ROOT/RDF/InterfaceUtils.hxx"
 #include "ROOT/RDF/RInterface.hxx"
 #include "ROOT/RDF/HistoModels.hxx"
 
-#include "FilterStore.h"
-
 using namespace ROOT;
 using RNode = ROOT::RDF::RNode;
-
 
 class Moller {
 
 public:
-    Moller(std::string files = "");
+   Moller(std::string files = "");
 
-    void Setup();
-    void Process();
-    void Print(Option_t *opt = "");
+   void Setup();
+   void Process();
+   void Print(Option_t *opt = "");
 
    RNode Pass(RNode in, bool exact = true);
+   RNode Add_Moller_Inv_Mass(RNode in, std::string pair_name="electron_pairs", std::string out_name="moller_inv_mass");
+   RNode Add_Momentum_Sum(RNode in, std::string pair_name="electron_pair", std::string out_name="el_pair_psum");
+   RNode Select_El_Pairs(RNode in, double time_cut=2., std::string out_name="electron_pairs");
+   RNode Anti_Select_El_Pairs(RNode in, double time_cut=4., std::string out_name="electron_pairs");
+   RNode Refine_El_Pairs_1(RNode in, double pmin=2.306*0.75, double pmax=2.306*1.15, std::string in_name="electron_pairs", std::string out_name="el_pairs_r1");
+   RNode Refine_El_Pairs_2(RNode in, std::string in_name="el_pairs_r1", std::string out_name="el_pairs_r2");
    RNode Cut_2_electrons(RNode in, bool exact = true);
 
+   // Variadic function permits any number of arguments. Unfortunately, the RDataFrame.Define or .Filter do not work with this.
+   template<typename ...Vecs>
+   static ROOT::VecOps::RVec<bool>
+   Combine_cuts_helper(ROOT::VecOps::RVec<bool> vec1, ROOT::VecOps::RVec<bool> vec2, Vecs ...vecs) {
+      /// Make a logic and combination of the input vectors and return result.
+      // The function makes use of C++17 variadic function templates and parameter packs, so that
+      // any number of input variables can be given. It works, but bot with RDataFrame.Define() :-(
 
+      RVec<bool> out;
+      std::vector<RVec<bool>> inputs;
+      (inputs.push_back(vecs), ...);  // C++17 parameter pack: push all argument vectors into a vector.
 
-    FilterStore &GetLastFilter() { return filters.back(); };
-    FilterStore &GetFilter(unsigned int i) { return filters[i]; };
-    FilterStore &GetFilter(std::string_view name) { return filters[filtermap[name]]; };
-    RNode GetLastFilterNode() { return filters.back().node; };
-    RNode GetFilterNode(unsigned int i) { return filters[i].node; };
-    RNode GetFilterNode(std::string_view name) { return filters[filtermap[name]].node; };
+      for (int i = 0; i < inputs[0].size(); ++i) { // Combine
+         // the inputs into the output.
+         bool is_ok = vec1[i] && vec2[i] && (vecs[i] && ...); // parameter pack expansion combines all ...vecs
+         out.push_back(is_ok);
+      }
+      return out;
+   }
 
-    // Helper Function
-    RNode FilterStep(RNode df, const std::string_view name = "", const std::string_view v0_status_name = "") {
-        std::string step_name, status_name;
-        if (name.empty()) {
-            step_name = df.GetFilterNames().back();
-        } else {
-            step_name = name;
-        }
-        filters.emplace_back(df, step_name, v0_status_name);
-        filtermap[name] = filters.size() - 1;
-        return df;
-    }
-    // Define specific filter steps.
+   static RVec<bool> Combine_cuts_helper2(RVec<bool> vec1, RVec<bool> vec2) { // Permit any number of arguments.
+      RVec<bool> out;
+      for (int i = 0; i < vec1.size(); ++i) { // Combe the inputs into the output.
+         out.push_back(vec1[i] && vec2[i]);
+      }
+      return out;
+   }
 
-    // Variadic function permits any number of arguments. Unfortunately, the RDataFrame.Define or .Filter do not work with this.
-    template<typename ...Vecs>
-    static ROOT::VecOps::RVec<bool>
-    Combine_cuts_helper(ROOT::VecOps::RVec<bool> vec1, ROOT::VecOps::RVec<bool> vec2, Vecs ...vecs) {
-        /// Make a logic and combination of the input vectors and return result.
-        // The function makes use of C++17 variadic function templates and parameter packs, so that
-        // any number of input variables can be given. It works, but bot with RDataFrame.Define() :-(
+   static RVec<bool>
+   Combine_cuts_helper3(RVec<bool> vec1, RVec<bool> vec2, RVec<bool> vec3) { // Permit any number of arguments.
+      RVec<bool> out;
+      for (int i = 0; i < vec1.size(); ++i) { // Combe the inputs into the output.
+         out.push_back(vec1[i] && vec2[i] && vec3[i]);
+      }
+      return out;
+   }
 
-        RVec<bool> out;
-        std::vector<RVec<bool>> inputs;
-        (inputs.push_back(vecs), ...);  // C++17 parameter pack: push all argument vectors into a vector.
+   static RVec<bool> Combine_cuts_helper4(RVec<bool> vec1, RVec<bool> vec2, RVec<bool> vec3,
+                                          RVec<bool> vec4) { // Permit any number of arguments.
+      RVec<bool> out;
+      for (int i = 0; i < vec1.size(); ++i) { // Combe the inputs into the output.
+         out.push_back(vec1[i] && vec2[i] && vec3[i] && vec4[i]);
+      }
+      return out;
+   }
 
-        for (int i = 0; i < inputs[0].size(); ++i) { // Combine
-            // the inputs into the output.
-            bool is_ok = vec1[i] && vec2[i] && (vecs[i] && ...); // parameter pack expansion combines all ...vecs
-            out.push_back(is_ok);
-        }
-        return out;
-    }
+   static RVec<double> Vector_magnitude(RVec<double> x, RVec<double> y, RVec<double> z) {
+      /// Return the magnitude of the x,y,z vector for each entry in x,y,z.
+      RVec<double> out;
+      for (int i = 0; i < x.size(); ++i) {
+         out.push_back(sqrt(x[i] * x[i] + y[i] * y[i] + z[i] * z[i]));
+      }
+      return out;
+   }
 
-    static RVec<bool> Combine_cuts_helper2(RVec<bool> vec1, RVec<bool> vec2) { // Permit any number of arguments.
-        RVec<bool> out;
-        for (int i = 0; i < vec1.size(); ++i) { // Combe the inputs into the output.
-            out.push_back(vec1[i] && vec2[i]);
-        }
-        return out;
-    }
+   static RNode Combine_cuts(RNode in, std::string_view out_name, std::vector<std::string> cuts) {
+      ///
+      /// For the in RNode, Combine the named bool columns cuts into a single new named column 'out_name' using logical and (&&).
+      /// Example:
+      /// auto combo = Combine_cuts(dataframe, "v0_combo_cut", {"v0_clus_time_cut", "v0_d0_cut", "v0_track_clus_time_cut"} )
+      /// this will combine "v0_clus_time_cut", "v0_d0_cut", "v0_track_clus_time_cut" item for item into "v0_combo_cut".
+      ///
+      // Note: this can also be implemented as a template<typename ...Vecs> Combine_cuts(RNode in, std::string_view out_name, ...vecs)
+      // Unfortunately the RDataFrame.Define() seems to not like being passed a variadic function, so we need the switch either way.
+      //
+      switch (cuts.size()) {
+         case 0:
+         case 1:
+            std::cout << "ERROR - You need at least two columns to combine.";
+            break;
+         case 2:
+            return in.Define(out_name, Combine_cuts_helper2, cuts);
+         case 3:
+            return in.Define(out_name, Combine_cuts_helper3, cuts);
+         case 4:
+            return in.Define(out_name, Combine_cuts_helper4, cuts);
+            break;
+         default:
+            std::cout << "I did not expect anyone to ever need to cut " << cuts.size() << " all at once!\n";
+      }
+      return in;
+   }
 
-    static RVec<bool>
-    Combine_cuts_helper3(RVec<bool> vec1, RVec<bool> vec2, RVec<bool> vec3) { // Permit any number of arguments.
-        RVec<bool> out;
-        for (int i = 0; i < vec1.size(); ++i) { // Combe the inputs into the output.
-            out.push_back(vec1[i] && vec2[i] && vec3[i]);
-        }
-        return out;
-    }
-
-    static RVec<bool> Combine_cuts_helper4(RVec<bool> vec1, RVec<bool> vec2, RVec<bool> vec3,
-                                           RVec<bool> vec4) { // Permit any number of arguments.
-        RVec<bool> out;
-        for (int i = 0; i < vec1.size(); ++i) { // Combe the inputs into the output.
-            out.push_back(vec1[i] && vec2[i] && vec3[i] && vec4[i]);
-        }
-        return out;
-    }
-
-    static RVec<double> Vector_magnitude(RVec<double> x, RVec<double> y, RVec<double> z) {
-        /// Return the magnitude of the x,y,z vector for each entry in x,y,z.
-        RVec<double> out;
-        for (int i = 0; i < x.size(); ++i) {
-            out.push_back(sqrt(x[i] * x[i] + y[i] * y[i] + z[i] * z[i]));
-        }
-        return out;
-    }
-
-    static RNode Combine_cuts(RNode in, std::string_view out_name, std::vector<std::string> cuts) {
-        ///
-        /// For the in RNode, Combine the named bool columns cuts into a single new named column 'out_name' using logical and (&&).
-        /// Example:
-        /// auto combo = Combine_cuts(dataframe, "v0_combo_cut", {"v0_clus_time_cut", "v0_d0_cut", "v0_track_clus_time_cut"} )
-        /// this will combine "v0_clus_time_cut", "v0_d0_cut", "v0_track_clus_time_cut" item for item into "v0_combo_cut".
-        ///
-        // Note: this can also be implemented as a template<typename ...Vecs> Combine_cuts(RNode in, std::string_view out_name, ...vecs)
-        // Unfortunately the RDataFrame.Define() seems to not like being passed a variadic function, so we need the switch either way.
-        //
-        switch (cuts.size()) {
-            case 0:
-            case 1:
-                std::cout << "ERROR - You need at least two columns to combine.";
-                break;
-            case 2:
-                return in.Define(out_name, Combine_cuts_helper2, cuts);
-            case 3:
-                return in.Define(out_name, Combine_cuts_helper3, cuts);
-            case 4:
-                return in.Define(out_name, Combine_cuts_helper4, cuts);
-                break;
-            default:
-                std::cout << "I did not expect anyone to ever need to cut " << cuts.size() << " all at once!\n";
-        }
-        return in;
-    }
-
-    static std::string GetVersion(){
-        return( std::string(__MOLLER_VERSION__));
-    };
+   static std::string GetVersion(){
+      return( std::string(__MOLLER_VERSION__));
+   };
 
 public:
-    TChain ch{TChain("MiniDST")};
-    RDataFrame dataframe{RDataFrame(ch)};
-    std::map<std::string_view, unsigned int> filtermap;
-    std::vector<FilterStore> filters;
+   TChain ch{TChain("MiniDST")};
+   RDataFrame dataframe{RDataFrame(ch)};
 
 };
 

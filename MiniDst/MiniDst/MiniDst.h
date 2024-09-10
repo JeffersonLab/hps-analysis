6c82f7369c39d996320ba46cd9facfdbb11b55f6
Maurik Holtrop <maurik@physics.unh.edu>
2024-09-10 18:49:34 +0200

    Version update

--- MiniDst/MiniDst/MiniDst.h
+++ MiniDst/MiniDst/MiniDst.h
@@ -22,7 +22,7 @@
 #include "ROOT/RVec.hxx"
 #include "ROOT/RDF/InterfaceUtils.hxx"
 
-#define MiniDst_Version_ "1.2.0"
+#define MiniDst_Version_ "1.2.1"
 //
 // The following construction defines a "Variant", a type in C++17 and later that can contain different kinds of object.
 // In our case the variant contains each of the possible types that we have in the output tree.
